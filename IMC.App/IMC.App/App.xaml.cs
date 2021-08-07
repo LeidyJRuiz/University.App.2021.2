663740a606a8270d184c8f2ae23cf652549ee3ab
Joanna <Joanna@DESKTOP-A3T2OON>
2021-08-07 15:47:56 -0500

    Agregar archivos de proyecto.

---
+++ IMC.App/IMC.App/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using IMC.App.Views;
+
+namespace IMC.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new IndexPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
